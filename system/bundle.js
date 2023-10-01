2d88ec42edd60e60fbbef225545a8af7c0963112
luckycod3r <belovtimofei@gmail.com>
2023-10-01 17:19:36 +1100

    upd bundle system

---
+++ system/bundle.js
@@ -0,0 +1,35 @@
+  function prepareBundles(...classes) {
+    const combinedMethods = {};
+  
+    for (const Class of classes) {
+  
+      const prototype = Class.prototype;
+  
+  
+      for (const methodName of Object.getOwnPropertyNames(prototype)) {
+  
+        if (methodName !== 'constructor') {
+  
+          const method = prototype[methodName];
+  
+  
+          combinedMethods[methodName] = method;
+        }
+      }
+    }
+    class CombinedClass {
+      constructor() {
+        for (const ClassConstructor of classes) {
+            const instance = new ClassConstructor();
+            // Копируем свойства и методы экземпляра в CombinedClass
+            Object.assign(this, instance);
+          }
+      }
+    }
+  
+    Object.assign(CombinedClass.prototype, combinedMethods);
+  
+    return CombinedClass;
+}
+
+module.exports = {prepareBundles}
